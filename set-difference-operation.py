5026dae67c434df7978da9bf32463eaaa72d033f
vedha krishna <49089434+Vedha-Krishna-Yarasuri@users.noreply.github.com>
2019-09-30 18:21:02 +0530

    Create set-difference-operation.py

---
+++ set-difference-operation.py
@@ -0,0 +1,7 @@
+#  https://www.hackerrank.com/challenges/py-set-difference-operation/problem
+
+n1 = int(input())
+set_1 = set(map(int,input().split()))
+n2 = int(input())
+set_2 = set(map(int,input().split()))
+print(len(set_1-set_2))
