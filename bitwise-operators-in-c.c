0c0875faa5146893e09d0e41db20151755fc11d9
vedha krishna <49089434+Vedha-Krishna-Yarasuri@users.noreply.github.com>
2019-10-01 21:32:38 +0530

    Create bitwise-operators-in-c.c

---
+++ bitwise-operators-in-c.c
@@ -0,0 +1,21 @@
+// https://www.hackerrank.com/challenges/bitwise-operators-in-c/problem
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+//Complete the following function.
+
+
+void calculate_the_maximum(int n, int k) {
+  //Write your code here.
+}
+
+int main() {
+    int n, k;
+  
+    scanf("%d %d", &n, &k);
+    calculate_the_maximum(n, k);
+ 
+    return 0;
+}
