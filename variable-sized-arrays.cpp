4702d658dd183b0580586456636a35420de4503d
vedha krishna <49089434+Vedha-Krishna-Yarasuri@users.noreply.github.com>
2019-10-01 21:27:51 +0530

    Create variable-sized-arrays.cpp

---
+++ variable-sized-arrays.cpp
@@ -0,0 +1,40 @@
+// https://www.hackerrank.com/challenges/variable-sized-arrays/problem
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int n, q;
+    cin >> n >> q;
+
+    // create vector of vectors
+    vector<vector<int>> a(n);
+
+ 
+    for (int i = 0; i < n; i++) {
+   
+        int k;
+        cin >> k;
+
+   
+        a[i].resize(k);
+        for (int j = 0; j < k; j++) {
+            cin >> a[i][j];
+        }
+    }
+
+  
+    for (int q_num = 0; q_num < q; q_num++) {
+       
+        int i, j;
+        cin >> i >> j;
+        cout << a[i][j] << endl;
+    }
+    return 0;
+}
+
