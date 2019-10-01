eb06c26ccc3355b07802d1beda48402d9ccdfc21
vedha krishna <49089434+Vedha-Krishna-Yarasuri@users.noreply.github.com>
2019-10-01 21:18:31 +0530

    Create capitalize.py

---
+++ capitalize.py
@@ -0,0 +1,30 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# https://www.hackerrank.com/challenges/capitalize/problem
+
+# Complete the solve function below.
+def solve(s):
+    str1=""
+    s=s.split(" ")
+    for i in range(len(s)):
+        s[i]=s[i].capitalize()
+        str1=str1+str(s[i])+" "
+    return(str1)
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = solve(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
