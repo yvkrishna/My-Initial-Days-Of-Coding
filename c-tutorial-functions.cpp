e735dabc5286d6165a6bc785ce40e4b963d0dda1
vedha krishna <49089434+Vedha-Krishna-Yarasuri@users.noreply.github.com>
2019-10-03 22:51:14 +0530

    Create c-tutorial-functions.cpp

---
+++ c-tutorial-functions.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+// https://www.hackerrank.com/challenges/c-tutorial-functions/problem
+int max_of_four(int w, int x, int y, int z)
+{
+    if(w>x && w>y && w>z )
+    {
+        return w;
+    }
+    else if(x>w && x>y && x>z )
+    {
+        return x;
+    }
+    else if(y>w && y>x && y>z )
+    {
+        return y;
+    }
+    else
+    {
+        return z;
+    }
+}
+
+int main() {
+    int a, b, c, d;
+    cin>>a>>b>>c>>d;
+    int ans = max_of_four(a, b, c, d);
+    cout<<ans;
+    
+    return 0;
+}
+
