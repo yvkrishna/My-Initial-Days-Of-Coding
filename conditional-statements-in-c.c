e0f43a0b83ca9ec738fb05d59316d7b9f6668afe
vedha krishna <49089434+Vedha-Krishna-Yarasuri@users.noreply.github.com>
2019-10-04 18:05:24 +0530

    Create conditional-statements-in-c.c

---
+++ conditional-statements-in-c.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+
+// https://www.hackerrank.com/challenges/conditional-statements-in-c/problem
+
+int main()
+{
+    int a;
+   scanf("%d",&a);
+    if (a==1)
+    {
+        printf("one");
+    }
+    else if (a==2)
+    {
+        printf("two");
+    }
+ else if (a==3)
+    {
+      printf("three");
+    }
+    else if (a==4)
+    {
+        printf("four");
+    }
+    else if (a==5)
+    {
+        printf("five");
+    }
+    else if (a==6)
+    {
+        printf("six");
+    }
+    else if (a==7)
+    {
+        printf("seven");
+    }
+    else if (a==8)
+    {
+       printf("eight");
+    }else if (a==9)
+    {
+       printf("nine");
+    }
+    else 
+    {
+      printf("Greater than 9");
+    }
+    return 0;
+}
+
