eaddb7655ca6fd4ce54de5336f50e0f683c67884
vedha krishna <49089434+Vedha-Krishna-Yarasuri@users.noreply.github.com>
2019-10-04 17:52:48 +0530

    Create reverse-array-c.c

---
+++ reverse-array-c.c
@@ -0,0 +1,25 @@
+// https://www.hackerrank.com/challenges/reverse-array-c/problem
+
+#include <stdio.h>
+int main()
+{
+    int a,r,i,t;
+        scanf("%d",&a );
+        int n[a];
+    for(i=0;i<a;i++)
+    {
+        scanf("%d",&n[i]);
+    }
+    for(i=0;i<a/2;i++)
+    {
+        t=n[i];
+        n[i]=n[a-i-1];
+        n[a-1-i]=t;
+    }
+for (i=0;i<a;i++)
+{
+    printf("%d ",n[i]);
+}
+    return 0;
+}
+
