df6388cadea1b4b78df58913f634016ed2ff7ad5
vedha krishna <49089434+Vedha-Krishna-Yarasuri@users.noreply.github.com>
2019-10-08 21:55:48 +0530

    Create 1008A.c

---
+++ 1008A.c
@@ -0,0 +1,34 @@
+// https://codeforces.com/contest/1008/problem/A
+
+#include <stdio.h>
+#include <string.h>
+#define sf scanf
+#define pf printf
+int main()
+{
+		int i,n,flag=1;
+		char s[100];
+		scanf("%s",s);
+		n=strlen(s);
+		for(i=0;i<n;i++)
+		{
+				if( s[i]!='n' && (s[i]!='a' && s[i]!='e' && s[i]!='i' && s[i]!='o' && s[i]!='u'))
+				{
+					if(s[i+1]!='a' && s[i+1]!='e' && s[i+1]!='i' && s[i+1]!='o' && s[i+1]!='u')
+					{
+						pf("NO");
+						flag=0;
+						break;
+					}
+					else
+					flag=1;
+				}
+					
+		
+		}
+		if (flag==1)
+		{
+				pf("YES");
+		}
+		return 0;
+}
