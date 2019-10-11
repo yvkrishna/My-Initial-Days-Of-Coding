02f09d85748d904fe9ee777e6fd9b1390bc766d1
vedha krishna <49089434+Vedha-Krishna-Yarasuri@users.noreply.github.com>
2019-10-11 22:43:53 +0530

    Create 4A.c

---
+++ 4A.c
@@ -0,0 +1,17 @@
+// https://codeforces.com/contest/4/problem/A
+
+#include<stdio.h>
+int main()
+{
+int weight;
+scanf("%d",&weight);
+if ((weight)%2==0 && weight/2>1)
+{
+printf("YES");
+}
+else
+{
+printf("NO");
+}
+ 
+}
