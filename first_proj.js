51424ebe579fe5a9711a04ef6b53a62afa95a9d6
vedha krishna <49089434+Vedha-Krishna-Yarasuri@users.noreply.github.com>
2019-10-12 09:39:26 +0530

    Add files via upload

---
+++ first_proj.js
@@ -0,0 +1,59 @@
+var c=require("fs");/*
+var m=c.readFileSync("hello.txt");
+
+var ab=require("cowsay")
+var b=ab.say({
+	text:m.toString(),
+	e:'!!',
+	T:'ww',
+})
+console.log(b);*/
+
+
+/*var ab=require("cowsay")
+c.readFile("hello.txt",function(a,b)
+{
+	var b=ab.say({
+		text:b.toString(),
+		e:"O",
+		t:"W",
+	})
+	console.log(b);
+})
+console.log("hello");*/
+
+/*
+var ch=c.readFileSync("hello.txt")
+console.log(ch.toString()+"\n");
+console.log("hello");
+*/
+
+/*c.readFile("hello.txt",function(a,b)
+{
+	console.log(b.toString());
+})
+console.log("hello");*/
+
+
+/*var ab=require("cowsay");
+var m=c.readFileSync("hello.txt");
+var p=ab.say({
+	text:m.toString(),
+	e:"!!",
+	t:'w',
+})
+console.log(p);
+console.log("abc");*/
+
+/*var ab=require("cowsay");
+c.readFile("hello.txt",function(a,b)
+	{
+		var p=ab.say({
+			text:b.toString(),
+			e:"!!",
+			t:"W"
+		})
+		console.log(p);
+	});
+console.log("abc");
+*/
