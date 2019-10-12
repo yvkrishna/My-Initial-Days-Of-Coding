094aa14030cc3c482e4d6921e8af38381d605a1a
vedha krishna <49089434+Vedha-Krishna-Yarasuri@users.noreply.github.com>
2019-10-12 09:39:12 +0530

    Add files via upload

---
+++ express2_node.js
@@ -0,0 +1,11 @@
+var ab=require("express");
+var a=require("fs");
+var ch=ab();
+ch.use(ab.static("public"));
+ch.get("/a",function(req,res)
+{
+	res.sendFile(__dirname+"/public/login.html");
+})
+ch.listen(5000,function(){
+	console.log("jhdsvc");
+})
