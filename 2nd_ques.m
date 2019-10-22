7d51ffa5fd25d9037223e50667e9cd4287ba2f22
vedha krishna <49089434+Vedha-Krishna-Yarasuri@users.noreply.github.com>
2019-10-22 22:59:56 +0530

    Create 2nd_ques.m

---
+++ 2nd_ques.m
@@ -0,0 +1,36 @@
+y=[2,3,5,6,4,7];
+x=[0:1:5];​
+[x,y]=impulse2(-5,5);​
+​
+% H1​
+​
+b1=[1,0,0,5];​
+a1=[1,0,2,0];​
+c1=filter(b1,a1,y);​
+​
+%H2​
+​
+b2=[1,0,0.5];​
+a2=[1,0,0];​
+c2=filter(b2,a2,y);​
+​
+%H3​
+​
+b3=[0.3333,0.3333,0.3333];​
+a3=[1,0,0];​
+c3=filter(b3,a3,y);​
+​
+figure;​
+subplot(2,2,1);​
+stem(x,y);xlabel('t');ylabel('x ( t )');title('input signal - > impulse signal');​
+subplot(2,2,2);​
+stem(x,c1);xlabel('t');ylabel('h1 ( t )');title('impulse response for the h1 system');​
+subplot(2,2,3);​
+stem(x,c2);xlabel('t');ylabel('h2 ( t )');title('impulse response for the h2 system');​
+subplot(2,2,4);​
+stem(x,c3);xlabel('t');ylabel('h3 ( t )');title('impulse response for the h3 system');​
+​
+figure​
+y1=conv(c1,c2);​
+[x123,y2]=sigadd(c3,y1,-5,5,-10,10);​
+stem(x123,y2);;xlabel('t');ylabel('h ( t )');title('impulse response combined system')​
