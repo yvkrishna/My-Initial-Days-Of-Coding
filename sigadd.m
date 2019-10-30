7c73021d02ba69a75b02bbd8336d1dab1a866d1a
vedha krishna <49089434+yvkrishna@users.noreply.github.com>
2019-10-30 22:29:58 +0530

    Add files via upload

---
+++ sigadd.m
@@ -0,0 +1,8 @@
+function [y,n]=sigadd(x1,x2,n1,n2,n3,n4) 
+    n=min(n1,n3):max(n2,n4); 
+    y1=zeros(1,length(n)); 
+    y2=y1; 
+    y1(n>=n1 & n<=n2)=x1; 
+    y2(n>=n3 & n<=n4)=x2; 
+    y=y1+y2; 
+end 
