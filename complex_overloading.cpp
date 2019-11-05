eb23a84b15c7bca2e4ee0fdcd5b2a64b13b6f740
vedha krishna <49089434+yvkrishna@users.noreply.github.com>
2019-11-05 18:12:43 +0530

    Add files via upload

---
+++ complex_overloading.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+class Complex 
+{
+float real,imag;
+public:
+Complex ()
+{
+real=imag=0.0;	
+}	
+void setdata()
+{
+cout<<"\nenter real value ";
+	cin>>real;
+	cout<<"\n enter imag value \n";
+	cin>>imag;
+}
+void display(char *msg)
+{
+	cout<<msg<<"="<<real;
+	if (imag<0)
+	{
+	cout<<"-i";
+	}
+	else
+	{
+	cout<<"+i";	
+	}
+	cout<<fabs(imag)<<endl;
+}
+Complex operator +(Complex c);
+};
+Complex Complex :: operator +(Complex c)
+{
+Complex temp;
+temp.real=real+c.real;
+temp.imag=imag+c.real;	
+return temp;
+}
+
+int main ()
+{
+Complex c1,c2,c3;
+
+cout<<"\enter c1.......\n";
+c1.setdata();
+cout<<"\enter c2.......\n";
+c2.setdata();
+c1.display("c1");
+c2.display("c2");
+
+c3=c1+c2;
+
+c3.display("c3");
+return 0;
+}
