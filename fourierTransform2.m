1e686a5aa7770a0edb4cfcfecc9f85469aa102fa
vedha krishna <49089434+yvkrishna@users.noreply.github.com>
2020-01-20 22:46:15 +0530

    Add files via upload

--- fourierTransform2.m
+++ fourierTransform2.m
@@ -1,7 +1,10 @@
 function [output] = fourierTransform2(signal)
     output=zeros(size(signal));
+    nu=linspace(0,20,length(signal));
     n=0:length(signal)-1;
     for pneu=0:length(signal)-1
         output(pneu+1)=sum( ( signal.*exp( (-1i*2*pi*pneu.*n)/length(signal) ) ) );
     end
+    figure,
+    plot(nu,abs(output))
 end
